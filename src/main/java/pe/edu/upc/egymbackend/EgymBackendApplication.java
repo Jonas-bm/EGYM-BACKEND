27a96730c70c58d6cf20bf99f4251cad98e0cc9e
Jonas-bm <94211214+Jonas-bm@users.noreply.github.com>
2023-05-23 01:32:08 -0500

    Creación de mi repositorio

---
+++ src/main/java/pe/edu/upc/egymbackend/EgymBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.egymbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgymBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgymBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/egymbackend/EgymBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.egymbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgymBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

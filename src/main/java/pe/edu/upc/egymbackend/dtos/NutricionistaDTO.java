ccb65ea4d5be1d0709f1bc214a2d4924956ab1b3
AntonyFBV <antonybarreto06@gmail.com>
2023-05-25 21:13:59 -0500

    nuevo

--- src/main/java/pe/edu/upc/egymbackend/dtos/NutricionistaDTO.java
+++ src/main/java/pe/edu/upc/egymbackend/dtos/NutricionistaDTO.java
@@ -1,2 +1,78 @@
-package pe.edu.upc.egymbackend.dtos;public class NutricionistaDTO {
+package pe.edu.upc.egymbackend.dtos;
+
+import javax.persistence.Column;
+
+public class NutricionistaDTO {
+    private int idNutricionista;
+    private String nombreNutricionista;
+    private String apellidosNutricionista;
+    private String dni;
+    private String telefono;
+    private String sexo;
+    private String Descripcion;
+    private String Estado;
+
+    public int getIdNutricionista() {
+        return idNutricionista;
+    }
+
+    public void setIdNutricionista(int idNutricionista) {
+        this.idNutricionista = idNutricionista;
+    }
+
+    public String getNombreNutricionista() {
+        return nombreNutricionista;
+    }
+
+    public void setNombreNutricionista(String nombreNutricionista) {
+        this.nombreNutricionista = nombreNutricionista;
+    }
+
+    public String getApellidosNutricionista() {
+        return apellidosNutricionista;
+    }
+
+    public void setApellidosNutricionista(String apellidosNutricionista) {
+        this.apellidosNutricionista = apellidosNutricionista;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
 }
--- src/main/java/pe/edu/upc/egymbackend/services/INutricionistaService.java
+++ src/main/java/pe/edu/upc/egymbackend/services/INutricionistaService.java
@@ -1,2 +1,12 @@
-package pe.edu.upc.egymbackend.services;public interface INutricionistaService {
+package pe.edu.upc.egymbackend.services;
+
+import pe.edu.upc.egymbackend.entities.Nutricionista;
+
+import java.util.List;
+
+public interface INutricionistaService {
+    public void insert(Nutricionista nutricionista);
+    List<Nutricionista>list();
+    public void delete(int idNutricionista);
+    public Nutricionista listId(int idNutricionista);
 }
